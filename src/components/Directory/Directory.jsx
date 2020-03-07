84b7f641d23c3e6f2cb1a1dbe446339f79cb48e9
Sanjeet Tiwari <Sanjeet.Tiwari@ecs.co.uk>
2020-03-07 19:30:54 +0530

    Created a directory comp

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,9 +1,40 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import css from "./Directory.module.scss";
 
+const SearchField = () => {
+  return (
+    <div className={css.SearchField}>
+      <input
+        className={css.searchInput}
+        placeholder="Search for any video / folder"
+      />
+    </div>
+  );
+};
+
+const BackButton = () => {
+  return (
+    <button className={css.backbutton}>
+      <FontAwesomeIcon icon="arrow-left" />
+    </button>
+  );
+};
+
 const Directory = () => {
-  return <section className={css.directory}></section>;
+  return (
+    <section className={css.directory}>
+      <div className={css.dirNav}>
+        <div className={css.dirPath}>
+          <BackButton />
+          Home
+        </div>
+        <SearchField />
+      </div>
+    </section>
+  );
 };
 
 export default Directory;
---
+++ src/utils/initializeIcons.js
@@ -0,0 +1,8 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+function initializeIcons() {
+  library.add(faArrowLeft);
+}
+
+export default initializeIcons;
