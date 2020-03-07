da678333dc2c27dc3c9f0aec7668bb7841bb2938
Sanjeet Tiwari <Sanjeet.Tiwari@ecs.co.uk>
2020-03-07 18:44:38 +0530

    Frontend : Header is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,20 @@ const Header = () => {
         <div className={css.appName}>Almanac</div>
         <div className={css.appDesc}>Your own directory for Youtube videos</div>
       </section>
+      <section className={css.userInfo}>
+        <div className={css.userIntro}>
+          <div className={css.hello}>Hello</div>
+          <div className={css.userName}>Sanjeet Tiwari</div>
+          <div className={css.gateway} tabIndex={0}>
+            Logout
+          </div>
+        </div>
+        <img
+          className={css.userImage}
+          src="https://dummyimage.com/600x400/000/fff&text=X"
+          alt=""
+        />
+      </section>
     </section>
   );
 };
