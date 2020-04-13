0880b80ef290146d57a60d7762207cac4e849540
Sanjeet Tiwari <Sanjeet.Tiwari@ecs.co.uk>
2020-04-13 18:18:10 +0530

    added more fields to the folder details hook

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -3,14 +3,27 @@ import React, { useState } from "react";
 import css from "./Directory.module.scss";
 import useFolderDetails from "../../hooks/useFolderDetails";
 
-const TopBar = () => {
+const TopBar = ({ path, description, folderCount, fileCount }) => {
   return <div className={css.topBar}></div>;
 };
 
 const Directory = () => {
   const [currentFolder, setCurrentFolder] = useState("/");
-  const { description, files, folders, path } = useFolderDetails(currentFolder);
-  return <section className={css.directory}></section>;
+  const { folderName, description, files, folders, path } = useFolderDetails(
+    currentFolder
+  );
+  console.log(folderName);
+  return (
+    <section className={css.directory}>
+      <TopBar
+        folderName={folderName}
+        path={path}
+        description={description}
+        folderCount={folders ? folders.length : 0}
+        fileCount={files ? files.length : 0}
+      />
+    </section>
+  );
 };
 
 export default Directory;
--- src/hooks/useFolderDetails.js
+++ src/hooks/useFolderDetails.js
@@ -4,20 +4,36 @@ import { getFolders, getFiles } from "../api/directory.Api";
 const useFolderDetails = (folderID) => {
   const [folderDetails, setFolderDetails] = useState({});
 
+  function getFolderPath(folders, folderID) {
+    let path = [];
+    function getPath(folderID) {
+      const fol = folders.find((folder) => folder.id === folderID);
+      if (!fol.parent) return;
+      path.push({
+        name: fol.name,
+        id: fol.id,
+      });
+      getPath(fol.parent);
+    }
+    getPath(folderID);
+    return path.reverse();
+  }
+
   useEffect(() => {
     async function getRecords() {
-      let path = [];
-
       const folders = await getFolders();
       const currentFolder = folders.find((folder) => folder.id === folderID);
+      let folderName = currentFolder.name ?? "Home";
       let description =
         currentFolder.description ?? "No description found for this folder.";
       let folderArray = folders.filter((folder) => folder.parent === folderID);
+      let path = getFolderPath(folders, folderID);
 
       const files = await getFiles();
       let fileArray = files.filter((file) => file.parent === folderID);
 
       setFolderDetails({
+        folderName,
         path,
         description,
         folders: folderArray,
