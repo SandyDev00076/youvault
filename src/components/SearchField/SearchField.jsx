01eda74377e07a55f771d601e8b47c07ab587e5a
Sanjeet Tiwari <Sanjeet.Tiwari@ecs.co.uk>
2020-04-22 10:49:40 +0530

    perfected search field

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -8,7 +8,13 @@ import fileTypeUtils from "../../utils/fileTypes";
 
 const SearchField = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [dropdownVisibility, setDropdownVisibility] = useState(false);
   const { searchResult } = useSearchQuery({ query: searchQuery });
+
+  function clearField() {
+    setDropdownVisibility(false);
+    setSearchQuery("");
+  }
 
   return (
     <div className={css.searchWrapper}>
@@ -17,27 +23,41 @@ const SearchField = () => {
         placeholder="Search for files / folders"
         value={searchQuery}
         onChange={(evt) => setSearchQuery(evt.target.value)}
+        onFocus={() => setDropdownVisibility(true)}
       />
       {searchQuery && (
-        <div className={css.searchDropdown}>
-          {searchResult.map((item, index) => (
-            <Link
-              key={index}
-              to={`/folders/${item.idToGoto !== "/" ? item.idToGoto : ""}`}
-              className={css.searchDropdownItem}
-              onClick={() => setSearchQuery("")}
-            >
-              <div className={css.itemDetails}>
-                <FontAwesomeIcon
-                  icon={fileTypeUtils[item.type].icon}
-                  className={css.itemTypeIcon}
-                />
-                <div className={css.itemType}>{item.type}</div>
-              </div>
-              <div className={css.itemName}>{item.name}</div>
-            </Link>
-          ))}
-        </div>
+        <FontAwesomeIcon
+          icon="times"
+          className={css.closeBtn}
+          onClick={clearField}
+        />
+      )}
+      {dropdownVisibility && searchQuery && (
+        <>
+          <div
+            className={css.backgroundDiv}
+            onClick={() => setDropdownVisibility(false)}
+          ></div>
+          <div className={css.searchDropdown}>
+            {searchResult.map((item, index) => (
+              <Link
+                key={index}
+                to={`/folders/${item.idToGoto !== "/" ? item.idToGoto : ""}`}
+                className={css.searchDropdownItem}
+                onClick={clearField}
+              >
+                <div className={css.itemDetails}>
+                  <FontAwesomeIcon
+                    icon={fileTypeUtils[item.type].icon}
+                    className={css.itemTypeIcon}
+                  />
+                  <div className={css.itemType}>{item.type}</div>
+                </div>
+                <div className={css.itemName}>{item.name}</div>
+              </Link>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
