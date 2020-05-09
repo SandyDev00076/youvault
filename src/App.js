618f0389480e90f005315594adcd58b3bad50fb3
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2020-05-09 21:20:29 +0530

    Built a login page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/globals.scss";
-import Header from "./components/Header";
+import Directory from "./components/Directory";
+import Login from "./components/Login";
 
 import "./App.scss";
-import Directory from "./components/Directory";
 
 function App() {
   return (
@@ -16,6 +16,9 @@ function App() {
         </Route>
         <Route exact path="/folders">
           <Directory />
+        </Route>
+        <Route exact path="/login">
+          <Login />
         </Route>
       </Switch>
     </Router>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 
 import SimpleLayout from "../../layouts/SimpleLayout";
+import { AppLogo } from "../Header";
+import Label from "../atoms/Label";
+import Input from "../atoms/Input";
+import Button from "../atoms/Button";
+import Link from "../atoms/Link";
 
 import css from "./Login.module.scss";
 
 const Login = () => {
-  return <SimpleLayout></SimpleLayout>;
+  function logIn(evt) {
+    evt.preventDefault();
+  }
+
+  return (
+    <SimpleLayout componentClass={css.login}>
+      <div className={css.welcome}>
+        <div className={css.welcomeText}>Log in</div>
+        <AppLogo />
+      </div>
+      <form className={css.loginForm} onSubmit={(evt) => logIn(evt)}>
+        <Label name="Username">
+          <Input />
+        </Label>
+        <Label name="Password">
+          <Input />
+        </Label>
+        <Button type="primary" className={css.loginBut}>
+          Log in
+        </Button>
+      </form>
+      <div className={css.loginOptions}>
+        <Link>Forgot Password?</Link>
+        <Link>Create an account</Link>
+      </div>
+    </SimpleLayout>
+  );
 };
 
 export default Login;
